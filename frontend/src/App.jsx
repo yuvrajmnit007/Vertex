9426330c7b1f4c3510dcfaa5edf551842e818698
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-08-22 00:06:46 +0530

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
-import { Routes,Route,useLocation } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Home from './pages/Home'
-import Movies from './pages/Movies'
-import SeatLayout from './pages/SeatLayout'
-import MyBookings from './pages/MyBookings'
-import Favorites from './pages/Favorites'
-import {Toaster} from 'react-hot-toast'
-import Footer from './components/Footer'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetails from "./pages/MovieDetails";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorites";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddShows from "./pages/admin/AddShows";
+import ListShows from "./pages/admin/ListShows";
+import ListBookings from "./pages/admin/ListBookings";
+import { useAppContext } from "./context/AppContext";
+// import { SignIn } from "@clerk/clerk-react";
+import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
 
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
 
-function App() {
-  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  const { user } = useAppContext();
 
   return (
     <>
+      <Toaster />
       {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies ' element={<Movies />} />
-        <Route path='/movies/:id' element={<SeatLayout />} /> 
-        <Route path='/movies/:id/:date' element={<SeatLayout />} />
-        <Route path='/mybookings' element={<MyBookings />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/loading/:nextUrl" element={<Loading />} />
+        <Route path="/favorite" element={<Favorite />} />
 
+        {/* Admin Routes */}
+        <Route
+          path="/admin/*"
+          element={
+            user ? (
+              <Layout />
+            ) : (
+              <div className="min-h-screen flex justify-center items-center">
+                <SignIn fallbackRedirectUrl={"/admin"} />
+              </div>
+            )
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
+      </Routes>
       {!isAdminRoute && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/admin/ListShows.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListShows = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListShows
