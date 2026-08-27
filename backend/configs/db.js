c17efea0bbfcdcdfdc4bdb07850553220279e679
yuvrajchoudhary3768@gmail.com <yuvrajchoudhary3768@gmail.com>
2026-08-28 00:30:17 +0530

    stripe payement method added

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/Vertex`);
   } catch (error) {
     console.log(error.message);
   }
